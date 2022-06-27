7e224e1a74964c6e53ac3c415f2ba80c72a8195f
roshan singh <roshansingh@deloitte.com>
2022-06-27 16:04:54 +0530

    API gateway testing run 8

--- gcp-apigateway-build.sh
+++ gcp-apigateway-build.sh
@@ -17,13 +17,13 @@ gcloud services enable servicecontrol.googleapis.com
 gcloud services enable iap.googleapis.com
 
 #Create the API gateway config by running the following command
-gcloud api-gateway api-configs create gcp-cms-api-config-new --api=$API_NAME --openapi-spec=$CONFIG_FILE --project=$PROJECT_ID
+gcloud api-gateway api-configs create gcp-sow-api-config-new --api=$API_NAME --openapi-spec=$CONFIG_FILE --project=$PROJECT_ID
 
 #deploy the API config to a gateway, run the below command
-gcloud api-gateway gateways create gcp-cms-api-gateway --api=$API_NAME --api-config=gcp-cms-api-config-new --location=us-east4 --project=$PROJECT_ID
+gcloud api-gateway gateways create gcp-sow-api-gateway --api=$API_NAME --api-config=gcp-sow-api-config-new --location=us-east4 --project=$PROJECT_ID
 
 #run the below commands to view details about the gateway
-gcloud api-gateway gateways describe gcp-cms-api-gateway --location=us-east4 --project=$PROJECT_ID
+gcloud api-gateway gateways describe gcp-sow-api-gateway --location=us-east4 --project=$PROJECT_ID
 
 #Enable the created api in API & Service page
 #MANAGED_SERVICE_NAME=$(gcloud api-gateway apis describe gcpcmsauth --format="value(managedService)")
