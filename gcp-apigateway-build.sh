97e9ef08d3e15c5ec18f8126a9be8857492e14b2
roshan singh <roshansingh@deloitte.com>
2022-07-01 17:09:23 +0530

    Refactor the code as per branches

--- gcp-apigateway-build.sh
+++ gcp-apigateway-build.sh
@@ -6,6 +6,8 @@
 PROJECT_ID=$1
 API_NAME=$2
 CONFIG_FILE=$3
+APIGATEWAY=$4
+CONFIG_NAME=$5
 
 #Enable required APIs in Cloud Shell, run the following commands to enable the required service
 gcloud config set project $PROJECT_ID
@@ -17,14 +19,14 @@ gcloud services enable servicecontrol.googleapis.com
 gcloud services enable iap.googleapis.com
 
 #Create the API gateway config by running the following command
-gcloud api-gateway api-configs create gcp-fast-api-config --api=$API_NAME --openapi-spec=$CONFIG_FILE --project=$PROJECT_ID
+gcloud api-gateway api-configs create $CONFIG_NAME --api=$API_NAME --openapi-spec=$CONFIG_FILE --project=$PROJECT_ID
 
 #deploy the API config to a gateway, run the below command
-gcloud api-gateway gateways create gcp-fast-api-gateway-new --api=$API_NAME --api-config=gcp-fast-api-config --location=us-east4 --project=$PROJECT_ID
+gcloud api-gateway gateways create $APIGATEWAY --api=$API_NAME --api-config=$CONFIG_NAME --location=us-east4 --project=$PROJECT_ID
 
 #run the below commands to view details about the gateway
-gcloud api-gateway gateways describe gcp-fast-api-gateway-new --location=us-east4 --project=$PROJECT_ID
+gcloud api-gateway gateways describe $APIGATEWAY --location=us-east4 --project=$PROJECT_ID
 
 #Enable the created api in API & Service page
-#MANAGED_SERVICE_NAME=$(gcloud api-gateway apis describe gcpcmsauth --format="value(managedService)")
-#gcloud services enable $MANAGED_SERVICE_NAME
+MANAGED_SERVICE_NAME=$(gcloud api-gateway apis describe $API_NAME --format="value(managedService)")
+gcloud services enable $MANAGED_SERVICE_NAME
